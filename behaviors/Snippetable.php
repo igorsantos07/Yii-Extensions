8952ef262b158b706e34479e4ebb04b76501ecda
igoru <igorsantos07@gmail.com>
2011-09-23 23:41:29 -0300

    adding entity decode

--- behaviors/Snippetable.php
+++ behaviors/Snippetable.php
@@ -10,15 +10,15 @@ class Snippetable extends CBehavior {
 
 	function snippet($attr, $size = self::DEFAULT_LENGHT, $ellipsis = '...') {
      	if (!$size) $size = self::DEFAULT_LENGHT;
-		
+
 		if (is_string($this->owner->$attr)) {
         	if (strlen($this->owner->$attr) <= $size)
 				return $this->owner->$attr;
 			else {
 				if ($ellipsis)
-					return substr(strip_tags($this->owner->$attr), 0, $size-(strlen($ellipsis))).$ellipsis;
+					return substr(strip_tags(html_entity_decode($this->owner->$attr)), 0, $size-(strlen($ellipsis))).$ellipsis;
 				else
-					return substr(strip_tags($this->owner->$attr), 0, $size);
+					return substr(strip_tags(html_entity_decode($this->owner->$attr)), 0, $size);
 			}
 		}
 	}
